7dbd31148c452f956a465a21c6e0bca8d968ec12
John Mason <johnmsn22@gmail.com>
2025-03-23 12:47:08 +0100

    update build

--- dist/index.js
+++ dist/index.js
@@ -19,9 +19,9 @@ const mongoDb_1 = __importDefault(require("./db/mongoDb"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: process.env.ALLOWED_ORIGINS }));
+app.use((0, cors_1.default)({ origin: ["http://localhost:3000"] }));
 app.use(express_1.default.json());
-app.use("/api/v1", (req, res) => {
+app.use("/", (req, res) => {
     res.status(200).json({ message: "Backend server is running..." });
 });
 app.use("*", (req, res) => {
